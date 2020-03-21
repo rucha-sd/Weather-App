be6e12c531ade4eda8104831a3a377e7fc6f8ebb
Rucha <rucha.d2000@gmail.com>
2020-03-21 15:25:46 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDirectoryPath = path.join(__dirname , '../public')
 const viewsPath = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -67,11 +69,7 @@ app.get('/weather', (req, res) =>{
             })
           })
     })
-    // res.send({
-    //     forecast: 'It is snowing',
-    //     location: 'Philadelphia',
-    //     address: req.query.address
-    // })
+
 })
 
 app.get('/help/*', (req, res) => {
@@ -90,6 +88,6 @@ app.get('*', (req, res) => {
 
     })
 })
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log('Server is up on port 3000.')
 })
